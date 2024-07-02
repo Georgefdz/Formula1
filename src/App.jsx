dcfd9616a1991ccd762ccd37d9b00f085fa03003
Georgefdz <georgfdz@gmail.com>
2024-07-01 19:23:53 -0600

    Feat: drivers info in each card

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,45 @@ import DriversCard from './DriversCard';
 import NavBar from './NavBar';
 
 function App() {
-  let [data, setData] = useState([]);
+  let [driversData, setDriversData] = useState([]);
+  let [driversData2, setDriversData2] = useState([]);
   let [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-useEffect(() => {
-  fetch("https://api.openf1.org/v1/drivers?session_key=latest")
-  .then(res => {
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('http://ergast.com/api/f1/current/driverStandings.json');
+        const response2 = await fetch ('https://api.openf1.org/v1/drivers?session_key=latest');
+        let driversInfo = await response.json();
+        let driversInfo2 = await response2.json();
+        // console.log(driversInfo.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+        setDriversData(driversInfo.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+        setDriversData2(driversInfo2)
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
     }
-    return res.json();
-  })
-  .then(data => {
-    setData(data);
-    setLoading(false);
-  })
-  .catch(error => {
-    setError(error);
-    setLoading(false);
-  })
+
+  }
+  fetchData();
 }, [])
+
+
+
 
 if (loading) return <p>Loading...</p>;
 if (error) return <p>Error: {error.message}</p>;
-if (data.length === 0) return <p>No data available</p>;
+if (driversData.length === 0) return <p>No data available</p>;
 
   return (
     <>
       <NavBar />
       <h1>Drivers</h1>
       <div className='driver-card-container'>
-        {data.map((driver) => (
-          <DriversCard key={driver.driver_id} data={driver}/>
-        ))}
+          <DriversCard data={driversData} data2={driversData2}/>
       </div>
 
     </>
--- src/DriversCard.jsx
+++ src/DriversCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
-function DriversCard({data}) {
+function DriversCard({data, data2}) {
+ 
+
+    console.log(data2)
   return (
+    <>
+        {data.map((data) => (
         <div className='card-container'>
-            <div className='titles'>Name: <span>{data.full_name}</span></div>
-            <div className='titles'>Country: <span>{data.country_code}</span></div>
-            <div className='titles'>Team: <span>{data.team_name}</span></div>
-            <div className='titles'>Number: <span>{data.driver_number}</span></div>
-            <div className='image-container titles'>
-                <img src={data.headshot_url} alt="" />
-            </div>
-            
+            <div className='titles'>Name: <span>{`${data.Driver.givenName}  ${data.Driver.familyName}`}</span></div>
+            <div className='titles'>Nationality: <span>{data.Driver.nationality}</span></div>
+            <div className='titles'>Team: <span>{data.Constructors[0].name}</span></div>
+            <div className='titles'>Number: <span>{data.Driver.permanentNumber}</span></div>
+            <div className='titles'>Position: <span>{data.position}</span></div>
+            <div className='titles'>Points: <span>{data.points}</span></div>
         </div>
+      ))}
+
+
+    </>
+
+
   )
 }
 
